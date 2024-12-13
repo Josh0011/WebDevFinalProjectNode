68a803132183dd7308b401038b127742c6040eb7
Josh0011 <joshuasharma4@gmail.com>
2024-12-12 19:49:05 -0500

    start

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import cors from "cors";
+import session from "express-session";
+import "dotenv/config";
+
+
+const app = express()
+
+app.use(cors({
+                 credentials: true,
+                 origin: process.env.NETLIFY_URL || "http://localhost:3000",
+             }
+));
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET || "kanbas",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+        domain: process.env.NODE_SERVER_DOMAIN,
+    };
+}
+app.use(session(sessionOptions));
+app.use(express.json());
+
+app.listen(process.env.PORT || 4000)
